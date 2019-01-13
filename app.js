3d24977a88f1b251d79d4a03fd3befbcb9c8faed
Patrick Culley <pbculley@gmail.com>
2019-01-13 00:20:15 -0800

    testing

--- app.js
+++ app.js
@@ -22,104 +22,50 @@ app.post("/", async function(req, res){
 	  limit: 10
 	}).then(response => {
 		var businesses = response.jsonBody.businesses;
-		var imgArr = []; 
-		var coNameArr = []; 
-		var hourArr = []; 
-		var idArray = [];  
-		var openHours; 
-		var id = businesses.forEach(el => {
-			idArray.push(el.id); 
-			});
-		var imgs = businesses.map(foo => {
-			imgArr.push(foo.image_url); 
-			});
-		var coName = businesses.map(el => {
-			coNameArr.push(el.name);
-			});
+		var ids = businesses.map(el => el.id);
+		var imgs = businesses.map(el => el.image_url);
+		var coName = businesses.map(el => el.name);
 		var searchResults = {
-			name: coNameArr,
+			name: coName,
 			location: [], 
-			imgs: imgArr,
+			imgs: imgs,
 			displayPhone: [],
-			hour: openHours
+			hours: hours
 		}
-		var delay = 1 * 1000;
-			// function findDay(y){
-			// 	var dayArr = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun",]; 
-			// 	return dayArr[y]; 
-			// 	 }
 
-			// function findTime(x){
-			// 	var pm = "" + (x - 1200);
-			// 	var am = "" + x; 
-			// 	if(x == 1200){
-			// 		return "12:00 p.m"; 
-			// 	}
-			// 	if( x >= 1200){
-			// 		return pm.replace(/00$/, ":") + pm.slice(-2) + " p.m";
-			// 	}
-			// 		return am.replace(/00$/, ":") + am.slice(-2) + " a.m.";
-			// 	}
-		var rest;
+		var hours; 
+		async function delay(ms){
+			return new Promise(resolve => {
+				setTimeout(resolve,ms)
+			})
+		}
 
-
-
-			for(var x = 0; x < businesses.length; x++){
-						searchResults.location.push(businesses[x].location['display_address']); 
-						searchResults.displayPhone.push(businesses[x].display_phone); 
-			}
-		
-// setTimeout returns promises
-// 't' represents the milliseconds
-		// function settimeoutfunc(t,v){
-		// 	return new Promise(function(resolve,reject){
-		// 		 return setTimeout(() => resolve.bind(t), v);
-		// 	}); 			
-		// }
-
-		// function testing(){
-		// 	return new Promise(function(resolve, reject){
-		// 		rest = "fooey";
-		// 		resolve(rest)
-		// 	})
-		// }
-		
-		// settimeoutfunc(2000,)
-
-
-
-
-// 		var newone = []; 
-// var poo = ["x","b","o","d","y"];
-// const decon = poo.reduce((accum,current) => {
-
-// 	console.log(current); 
-// })
-// return decon; 
-			// setTimeout(function(x){
-			// 	client.business(idArray[x]).then(response => {
-			// 	var hours = response.jsonBody.hours.map(el => {
-			// 		return el.open;
-			// 	});
-			// 	openHours = hours.map(lo => {
-			// 		for(var x = 0; x < lo.length; x++){
-			// 			return lo.map(hrs => {
-			// 				var start = Number(hrs.start); 
-			// 				var end = Number(hrs.end);
-			// 			return findDay(hrs.day) + " " + findTime(hrs.start) + " - " + findTime(hrs.end); 
-			// 			}); 
-			// 		}
-			// 	});	
-			// 	console.log(openHours); 
-			// 	});  
-		
-			// },delay*x,x)b
+		async function timeout(){
+			var hours = ids.map(el => {
+				return client.business(el).then(async response => {
+					var hours =  response.jsonBody.hours;
+					console.log(hours); 
+				}); 
+				return hours; 
+			})
+			var result = await hours; 
+			return result; 
+		}
 	
-res.render('search', {
-	imgArr:imgArr,
-	searchResults:searchResults,
-	hour: rest
-});
+		// async function testing(){
+		// 	await delay(2000); 
+		// 	test = "test 1";  
+		// }
+
+		async function results(){
+			var result = await timeout(); 
+			for(var x = 0; x < businesses.length; x++){
+					searchResults.location.push(businesses[x].location['display_address']); 
+					searchResults.displayPhone.push(businesses[x].display_phone); 
+			}
+			return result;
+		}
+	results().then(result => res.render('search', {searchResults:searchResults, hours: hours}));
 }).catch(e => {
   console.log(e);
 });
@@ -142,24 +88,44 @@ app.get("/cafes/new", function(req,res){
 app.listen(3000);
 
 
+			// setTimeout(function(x){
+			// 	client.business(idArray[x]).then(response => {
+			// 	var hours = response.jsonBody.hours.map(el => {
+			// 		return el.open;
+			// 	});
 
 
+			// 	openHours = hours.map(lo => {
+			// 		for(var x = 0; x < lo.length; x++){
+			// 			return lo.map(hrs => {
+			// 				var start = Number(hrs.start); 
+			// 				var end = Number(hrs.end);
+			// 			return findDay(hrs.day) + " " + findTime(hrs.start) + " - " + findTime(hrs.end); 
+			// 			}); 
+			// 		}
+			// 	});	
+			// 	console.log(openHours); 
+			// 	});  
+		
+			// },delay*x,x)
+
+						// function findDay(y){
+			// 	var dayArr = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun",]; 
+			// 	return dayArr[y]; 
+			// 	 }
+
+			// function findTime(x){
+			// 	var pm = "" + (x - 1200);
+			// 	var am = "" + x; 
+			// 	if(x == 1200){
+			// 		return "12:00 p.m"; 
+			// 	}
+			// 	if( x >= 1200){
+			// 		return pm.replace(/00$/, ":") + pm.slice(-2) + " p.m";
+			// 	}
+			// 		return am.replace(/00$/, ":") + am.slice(-2) + " a.m.";
+			// 	}
 
 
-	
-
-
-// Create promises inside loop 
-function newone(){
-
-for(var i = 0; i < 5; i++){
-var promise = new Promise(function(resolve, reject){
-	setTimeout(() => {
-		console.log(i);
-	})
-})
-}
-promise.then(x => console.log(x)); 
-}
 
 
